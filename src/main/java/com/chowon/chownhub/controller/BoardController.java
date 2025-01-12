b51753b5363032128f6b9b1e95bf10f7bc58c2ae
jinwon <jinwon599@gmail.com>
2025-01-12 22:52:33 +0900

    컨트롤러 생성

--- src/main/java/com/chowon/chownhub/controller/BoardController.java
+++ src/main/java/com/chowon/chownhub/controller/BoardController.java
@@ -1,2 +1,30 @@
-package com.chowon.chownhub.controller;public class BoardController {
+package com.chowon.chownhub.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    private static final Logger log = LoggerFactory.getLogger(BoardController.class);
+    // 공지사항 출력
+    @GetMapping("/board/notice")
+    public String showNotice() {
+        log.debug("showNotice 메소드 호출됨");
+        return "/board/board_notice.html";
+    }
+
+    // 거래처 스펙 출력
+    @GetMapping("/board/specs")
+    public String showSpecs() {
+        return "/board/board_specs.html";
+    }
+
+    // 마음의 소리 출력
+    @GetMapping("/board/heart")
+    public String showHeart() {
+        return "/board/board_heart.html";
+    }
 }
--- src/main/java/com/chowon/chownhub/controller/UserController.java
+++ src/main/java/com/chowon/chownhub/controller/UserController.java
@@ -1,2 +1,42 @@
-package com.chowon.chownhub.controller;public class UserController {
+package com.chowon.chownhub.controller;
+
+import com.chowon.chownhub.member.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    // 로그인 화면 출력
+    @GetMapping("/user/login")
+    public String showLogin() {
+        System.out.println("xxxsexxxx");
+        log.debug("showLogin() 메소드 호출");
+        return "/user/login.html";
+    }
+
+    // 로그인 기능 구현
+    @PostMapping("/user/login")
+    public String userLogin(Member member) {
+        System.out.println("userLogin 호출");
+        log.debug("*****************login member : {}", member);
+        return "redirect:/";
+    }
+
+    // 회원가입 화면 출력
+    @GetMapping("/user/join")
+    public String showFrom() {
+
+        return "/user/form.html";
+    }
+
+    @PostMapping("/user/join")
+    public String createUser(Member member) {
+        log.debug("new member : {}", member);
+        return "redirect:/";
+    }
 }
