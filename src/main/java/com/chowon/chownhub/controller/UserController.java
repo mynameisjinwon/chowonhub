67a6f3a6e5ecd30f53ea1be64aa929b5ccaabe25
jinwon <jinwon599@gmail.com>
2025-01-13 21:58:37 +0900

    로그인 기능 구현

--- src/main/java/com/chowon/chownhub/controller/UserController.java
+++ src/main/java/com/chowon/chownhub/controller/UserController.java
@@ -1,29 +1,55 @@
 package com.chowon.chownhub.controller;
 
 import com.chowon.chownhub.member.Member;
+import com.chowon.chownhub.member.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.net.http.HttpResponse;
 
 @Controller
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    private MemberService service;
+
+    public UserController(MemberService service) {
+        this.service = service;
+    }
+
     // 로그인 화면 출력
     @GetMapping("/user/login")
     public String showLogin() {
-        System.out.println("xxxsexxxx");
-        log.debug("showLogin() 메소드 호출");
         return "/user/login.html";
     }
 
     // 로그인 기능 구현
     @PostMapping("/user/login")
-    public String userLogin(Member member) {
-        System.out.println("userLogin 호출");
-        log.debug("*****************login member : {}", member);
+    public String userLogin(Member member, HttpServletRequest request, Model model) {
+        if (!service.login(member)) {
+            log.debug("로그인 실패 : {}", member);
+            return "redirect:/user/login";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("loginMember", member);
+        model.addAttribute(member);
+        log.debug("로그인 성공 : {}", member.getMemberId());
+        return "redirect:/";
+    }
+
+    // 로그아웃
+    @GetMapping("/user/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+
         return "redirect:/";
     }
 
@@ -37,6 +63,7 @@ public class UserController {
     @PostMapping("/user/join")
     public String createUser(Member member) {
         log.debug("new member : {}", member);
+        service.join(member);
         return "redirect:/";
     }
 }
--- src/main/java/com/chowon/chownhub/member/MemberService.java
+++ src/main/java/com/chowon/chownhub/member/MemberService.java
@@ -2,7 +2,9 @@ package com.chowon.chownhub.member;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
     private static final Logger log = LoggerFactory.getLogger(MemberService.class);
 
@@ -16,10 +18,19 @@ public class MemberService {
         Member dbMember = memberRepository.findById(member.getMemberId());
         // 이미 존재하는 회원
         if (dbMember != null) {
-            log.debug("이미 존재하는 회원 아이디!");
-            return;
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
         memberRepository.save(member);
+    }
+
+    public boolean login(Member member) {
+        Member dbMember = memberRepository.findById(member.getMemberId());
+        // 존재하지 않는 회원
+        if (dbMember == null) {
+            return false;
+        }
+        // 잘못된 비밀번호를 입력하면
+        return dbMember.getPassword().equals(member.getPassword());
     }
 
     // 회원 아이디 찾기
